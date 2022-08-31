7bcdb036b013d42d334008dcf8705bed8d85e448
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-08-31 13:04:54 +0300

    модели

--- myconverter/mysite/polls/models.py
+++ myconverter/mysite/polls/models.py
@@ -1,56 +1,75 @@
 import uuid
 
 from django.db import models
+import datetime
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
 
-
-class File(TimeStampedModel):
-    id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4, editable=False)
-    file_path = models.FileField(_('File'), upload_to='files/')
-    resolution = models.CharField(_('Resolution'), max_length=100)
-    codec_name = models.CharField(_('Codec'), max_length=100, null=True, blank=True)
-    display_aspect_ratio = models.CharField(_('Display Aspect Ratio'), max_length=100, null=True, blank=True)
-    fps = models.IntegerField(_('FPS'), null=True, blank=True)
-
-    class Meta:
-        verbose_name = _('File')
-        verbose_name_plural = _('Files')
-        managed = False
-        # db_table = f'"content"."file"'
-        ordering = ["file_path"]
-
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
     def __str__(self):
-        return f'{self.file_path}'
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
-class FilmWork(TimeStampedModel):
-    id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(_('Name'), max_length=255)
-    certificate = models.TextField(_('Certificate'), blank=True)
-    file_path = models.FileField(_('Original file'), upload_to='film_works/')
-    files = models.ManyToManyField('movies.File', through='movies.FileFilmWork', related_name='filmworks')
-
-    class Meta:
-        verbose_name = _('Film')
-        verbose_name_plural = _('Films')
-        managed = False
-        # db_table = f'"content"."film_work"'
-        ordering = ["title"]
-
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
     def __str__(self):
-        return self.title
+        return self.choice_text
 
 
-class FileFilmWork(models.Model):
-    id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4, editable=False)
-    film_work = models.ForeignKey('movies.FilmWork', on_delete=models.CASCADE)
-    file = models.ForeignKey('movies.File', on_delete=models.CASCADE)
-    created = models.DateTimeField(_('Created'), auto_created=True, auto_now_add=True)
-
-    class Meta:
-        verbose_name = _('File')
-        verbose_name_plural = _('Files')
-        # db_table = f'"content"."file_film_work"'
-        managed = False
-        unique_together = ('film_work', 'file')
+#
+# class File(TimeStampedModel):
+#     id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4, editable=False)
+#     file_path = models.FileField(_('File'), upload_to='files/')
+#     resolution = models.CharField(_('Resolution'), max_length=100)
+#     codec_name = models.CharField(_('Codec'), max_length=100, null=True, blank=True)
+#     display_aspect_ratio = models.CharField(_('Display Aspect Ratio'), max_length=100, null=True, blank=True)
+#     fps = models.IntegerField(_('FPS'), null=True, blank=True)
+#
+#     class Meta:
+#         verbose_name = _('File')
+#         verbose_name_plural = _('Files')
+#         managed = False
+#         # db_table = f'"content"."file"'
+#         ordering = ["file_path"]
+#
+#     def __str__(self):
+#         return f'{self.file_path}'
+#
+#
+# class FilmWork(TimeStampedModel):
+#     id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4, editable=False)
+#     title = models.CharField(_('Name'), max_length=255)
+#     certificate = models.TextField(_('Certificate'), blank=True)
+#     file_path = models.FileField(_('Original file'), upload_to='film_works/')
+#     files = models.ManyToManyField('movies.File', through='movies.FileFilmWork', related_name='filmworks')
+#
+#     class Meta:
+#         verbose_name = _('Film')
+#         verbose_name_plural = _('Films')
+#         managed = False
+#         # db_table = f'"content"."film_work"'
+#         ordering = ["title"]
+#
+#     def __str__(self):
+#         return self.title
+#
+#
+# class FileFilmWork(models.Model):
+#     id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4, editable=False)
+#     film_work = models.ForeignKey('movies.FilmWork', on_delete=models.CASCADE)
+#     file = models.ForeignKey('movies.File', on_delete=models.CASCADE)
+#     created = models.DateTimeField(_('Created'), auto_created=True, auto_now_add=True)
+#
+#     class Meta:
+#         verbose_name = _('File')
+#         verbose_name_plural = _('Files')
+#         # db_table = f'"content"."file_film_work"'
+#         managed = False
+#         unique_together = ('film_work', 'file')
