df39cbc8492a5ae4d46e905734107473cd96ba83
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-08-31 15:50:03 +0300

    добавил модели для конвертации и прописал их в админке

--- myconverter/mysite/polls/admin.py
+++ myconverter/mysite/polls/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, File, FilmWork, FileFilmWork
 
 admin.site.register(Question)
+
+admin.site.register(File)
+admin.site.register(FilmWork)
+admin.site.register(FileFilmWork)
