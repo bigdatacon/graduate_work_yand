03cc5cea692ca2989c7f8f9c7d05512e40e215f7
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-09-01 20:53:47 +0300

    все 3 таблицы

--- myconverter/mysite/polls/admin.py
+++ myconverter/mysite/polls/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 # File, FilmWork, FileFilmWork,
-from .models import Question,  Choice, Fileupl , FilmWork
+from .models import Question,  Choice, Fileupl, FilmWork, FileFilmWork
 
 admin.site.register(Question)
 admin.site.register(Choice)
 
 # admin.site.register(File)
 admin.site.register(FilmWork)
-# admin.site.register(FileFilmWork)
+admin.site.register(FileFilmWork)
 admin.site.register(Fileupl)
