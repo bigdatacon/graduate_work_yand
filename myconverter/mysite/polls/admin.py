1917d5acddc7adaae6311ec372fa8bbbd3a02565
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-09-02 00:54:55 +0300

    удалил лишние модели

--- myconverter/mysite/polls/admin.py
+++ myconverter/mysite/polls/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 # File, FilmWork, FileFilmWork,
-from .models import Question,  Choice, Fileupl, FilmWork, FileFilmWork
+from .models import  Fileupl, FilmWork, FileFilmWork
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# admin.site.register(Question)
+# admin.site.register(Choice)
 
 # admin.site.register(File)
 admin.site.register(FilmWork)
--- myconverter/mysite/polls/models.py
+++ myconverter/mysite/polls/models.py
@@ -6,21 +6,21 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+#     def __str__(self):
+#         return self.question_text
+#     def was_published_recently(self):
+#         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+#
+#
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+#     def __str__(self):
+#         return self.choice_text
 
 class Fileupl(TimeStampedModel):
     id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4, editable=False)
