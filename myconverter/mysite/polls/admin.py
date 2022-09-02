2b36a6eab4d72a32ff8d93ba08456a80047505fa
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-09-02 13:49:43 +0300

    добавил inlines

--- myconverter/mysite/polls/admin.py
+++ myconverter/mysite/polls/admin.py
@@ -2,10 +2,22 @@ from django.contrib import admin
 # File, FilmWork, FileFilmWork,
 from .models import  Fileupl, FilmWork, FileFilmWork
 
-# admin.site.register(Question)
-# admin.site.register(Choice)
+class FileuplAdmin(admin.TabularInline):
+    model = Fileupl
 
-# admin.site.register(File)
-admin.site.register(FilmWork)
-admin.site.register(FileFilmWork)
+class FilmWorkAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    inlines = [FileuplAdmin]
+
+admin.site.register(FilmWork, FilmWorkAdmin)
 admin.site.register(Fileupl)
+
+
+
+# # admin.site.register(Question)
+# # admin.site.register(Choice)
+#
+# # admin.site.register(File)
+# admin.site.register(FilmWork)
+admin.site.register(FileFilmWork)
+# admin.site.register(Fileupl)
