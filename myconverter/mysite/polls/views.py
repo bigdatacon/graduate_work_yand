3d26b2d4f1fa913247f7e041efe0a4d9ebeb68bf
root <root@ovz4.j54523970.pv29m.vps.myjino.ru>
2022-09-03 23:54:09 +0300

    Index page

--- myconverter/mysite/polls/views.py
+++ myconverter/mysite/polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from polls.models import FilmWork
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    data = []
+    for film in FilmWork.objects.all():
+        film_info = {
+            'uuid': film.id,
+            'title': film.title,
+            'certificate': film.certificate,
+            'file_path': film.file_path.url,
+            'files': []
+        }
+        for furl in film.files.all():
+            film_info['files'].append(furl.file_path.url)
+        data.append(film_info)
+    return JsonResponse({'results': data})
