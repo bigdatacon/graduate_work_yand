b4ec34852fed02474a5c1d50e326053afd512641
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-09-04 01:10:29 +0300

    залил реквест из Ветки индекс

--- myconverter/mysite/polls/views.py
+++ myconverter/mysite/polls/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import FilmWork, Fileupl
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import FileuplSerializer
+
+
+class FileuplViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Fileupl.objects.all()
+    serializer_class = FileuplSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    data = []
+    for film in FilmWork.objects.all():
+        film_info = {
+            'uuid': film.id,
+            'title': film.title,
+            'certificate': film.certificate,
+            'file_path': film.file_path.url,
+            'files': []
+        }
+        for furl in film.files.all():
+            film_info['files'].append(furl.file_path.url)
+        data.append(film_info)
+    return JsonResponse({'results': data})
