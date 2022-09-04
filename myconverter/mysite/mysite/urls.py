6820870b85af094f9b2991604c6a019e1800f7ce
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-09-04 23:11:19 +0300

    залил реквест из Ветки индекс

--- myconverter/mysite/mysite/urls.py
+++ myconverter/mysite/mysite/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import include, path
 from rest_framework import routers
-from mysite.polls import views
+from polls import views
 
 router = routers.DefaultRouter()
 router.register(r'fileupload', views.FileuplViewSet)
