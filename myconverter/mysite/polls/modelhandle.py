fe7cb7c56f22fc3475de3ee2421f509b183abad6
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-09-07 10:41:25 +0300

    Добавил запросы в modelhandle.py

--- myconverter/mysite/polls/modelhandle.py
+++ myconverter/mysite/polls/modelhandle.py
@@ -2,7 +2,13 @@
 # from model_utils.models import TimeStampedModel
 # from django.conf import settings
 from .models import  Fileupl, FilmWork, FileFilmWork
+from .views import question_list
 # print(FilmWork.objects.all())
 
 def get_data_from_model(modelname):
     return FilmWork.objects.all()
+
+if __name__ == '__main__':
+    request = 'http://127.0.0.1:8000/polls/question/ question_text="print test"'
+    request_2 = '"http://127.0.0.1:8000/polls/question/3/", json={'question_text': "New text"}'
+    question_list(request)
