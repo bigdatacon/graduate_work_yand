66125cc012590531e78f79c4c400db8bcd63e68b
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-09-16 20:09:25 +0300

    модели для API в файле modelhandler

--- myconverter/mysite/polls/tests.py
+++ myconverter/mysite/polls/tests.py
@@ -55,17 +55,17 @@ class ConnectionTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
     #
     #4
-    def test_post_data(self):
-        """
-        Ensure we can create a new object in model .
-        """
-        # data = {'question_text': 'DabApps', 'files': {'file_path': self.fd}}
-        data = {'question_text': 'DabApps'}
-        # response = self.client.post('/polls/question/', data, format='json')
-        # response = self.client.post('/questions/', data, self.files)
-        response =self.client.post("/questions/", data, files=self.files)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Question.objects.get().question_text, 'DabApps')
+    # def test_post_data(self):
+    #     """
+    #     Ensure we can create a new object in model .
+    #     """
+    #     # data = {'question_text': 'DabApps', 'files': {'file_path': self.fd}}
+    #     data = {'question_text': 'DabApps'}
+    #     # response = self.client.post('/polls/question/', data, format='json')
+    #     response = self.client.post('/questions/', data, self.files)
+    #
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     self.assertEqual(Question.objects.get().question_text, 'DabApps')
 
 
     #5
@@ -88,37 +88,41 @@ class ConnectionTests(APITestCase):
 
 
     # #6
-    # def test_post_many_files(self):
-    #     """
-    #     Ensure we can create many new objects in model by loading many files from path.
-    #     """
-    #     files = []
-    #     i = 1
-    #     for p in Path(self.path).rglob('*'):
-    #         print(f' {i:} eto p : {str(p)}, type: {type(str(p))}')
-    #
-    #         file_path = str(p.parent) + p.name
-    #         print(f'eto file_path :')
-    #         print(file_path)
-    #         response = requests.post("http://127.0.0.1:8000/filmwork/",
-    #                                  {'title': f"test_api_{i}", 'certificate': f"test_api_{i}"},
-    #                                  files={'file_path': file_path})
-    #         print(f"Answer is with load_file_to_model {response.status_code}: {response.json()}")
-    #         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #         i += 1
+    def test_post_many_files(self):
+        """
+        Ensure we can create many new objects in model by loading many files from path.
+        """
+        files = []
+        i = 1
+        for p in Path(self.path).rglob('*'):
+            print(f' {i:} eto p : {str(p)}, type: {type(str(p))}')
+
+            file_path = str(p.parent) + p.name
+            print(f'eto file_path :')
+            print(file_path)
+            response = requests.post("http://127.0.0.1:8000/filmwork/",
+                                     {'title': f"test_api_{i}", 'certificate': f"test_api_{i}"},
+                                     files={'file_path': file_path})
+            # response = self.client.post(self.url_long,
+            #                          {'title': f"test_api_{i}", 'certificate': f"test_api_{i}"},
+            #                          files={'file_path': file_path})
+            # response = self.client.post(self.url_long,
+            #                          {'title': f"test_api_{i}", 'certificate': f"test_api_{i}", 'file_path': file_path})
+            # response = self.client.post(self.url,
+            #                          {'title': f"test_api_{i}", 'certificate': f"test_api_{i}", 'file_path': file_path})
+            print(f"Answer is with load_file_to_model {response.status_code}: {response.json()}")
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+            i += 1
     #
     # #7
-    # def test_can_update_film(self):
-    #     # {
-    #     #     "id": "c4563b47-e3a6-4ae0-a450-563901bde8e4",
-    #     #     "title": "test_api_1",
-    #     #     "certificate": "test_api_1",
-    #     #     "file_path": "http://127.0.0.1:8000/film_works/file_path_3MbT583"
-    #     # },
-    #     response = requests.put("http://127.0.0.1:8000/filmwork/c4563b47-e3a6-4ae0-a450-563901bde8e4/", {"title": "test_UPDATE", "certificate": "test"},
-    #                              files={'file_path': self.fd})
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     print(f'Answer after update : {response.json().get("title")}, send to update : {"test_UPDATE"}')
+    def test_can_update_film(self):
+        # response = requests.put("http://127.0.0.1:8000/filmwork/c4563b47-e3a6-4ae0-a450-563901bde8e4/", {"title": "test_UPDATE", "certificate": "test"},
+        #                          files={'file_path': self.fd})
+
+        response = self.client.put(f'{self.url}{self.test_filmwork_id}/', {"title": "test_UPDATE", "certificate": "test", 'file_path': self.fd})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(f'Answer after update : {response.json().get("title")}, send to update : {"test_UPDATE"}')
 
     def tearDown(self):
         self.fd.close()
