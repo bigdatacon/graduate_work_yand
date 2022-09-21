8ace46ab6784fae6b876ce8220e9a82b283a018a
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-09-21 17:37:58 +0300

    для докера

--- myconverter/converter/modelhandler.py
+++ myconverter/converter/modelhandler.py
@@ -112,7 +112,7 @@ class ModelHandler:
     def resize(self):
         stream =  ffmpeg.input(os.path.join("/usr", "src", "app", "тест.mp4"))
         stream = stream.filter('fps', fps=5, round = 'up').filter('scale', w=128, h=128)
-        stream = ffmpeg.output("NEW_MOVIE.mp4")
+        stream = ffmpeg.output(stream, "NEW_MOVIE.mp4")
         ffmpeg.run(stream)
 
 
