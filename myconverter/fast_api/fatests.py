f0567e549a5ef6fa663dc89f401b96478b240392
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-09-26 17:34:46 +0300

    работает шаблон для api

--- myconverter/fast_api/fatests.py
+++ myconverter/fast_api/fatests.py
@@ -1,5 +1,5 @@
 import requests
 print('начат main')
-answer = requests.get("http://127.0.0.1:8001/api/v1/apitesting")
-# answer = requests.get("http://127.0.0.1:8001/api/v1/modelhandlerapi")
+# answer = requests.get("http://127.0.0.1:8001/api/v1/apitesting")
+answer = requests.get("http://127.0.0.1:8001/api/v1/modelhandlerapi")
 print(f' eto answer one user: {answer.json()}')
