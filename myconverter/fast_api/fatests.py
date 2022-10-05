3baab91d6ad1aa2f9120bcee5a9b7de1aa18afaf
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-05 11:44:38 +0300

    правки в add_one_object_to_table

--- myconverter/fast_api/fatests.py
+++ myconverter/fast_api/fatests.py
@@ -57,25 +57,25 @@ except Exception as e:
 
 
 
-# answer = requests.get("http://127.0.0.1:8001/api/v1/modelhandlerapi/get_model_object_by_id/?film_uuid=efc94832-a392-4d68-b117-f90b5080218d")
-# file_path_to_convert = answer.json().get('file_path')
-# file_path_to_convert_from_stroka_15 = 'http://127.0.0.1:8000/media/film_works/%D1%82%D0%B5%D1%81%D1%82_GI7BI9C.mp4'
-# print(f' eto answer one user: {answer.json()}, file_path_to_convert : {file_path_to_convert} , {file_path_to_convert_from_stroka_15}')
-#
-#
-# converted_file_path = requests.post("http://127.0.0.1:8001/api/v1/modelhandlerapi/resize/?file_path_to_convert")
-# print(f' eto converted_file_path: {converted_file_path.json()}')
-#
-#
-# converted_file_path = requests.post("http://127.0.0.1:8001/api/v1/modelhandlerapi/resize/?file_path_to_convert_from_stroka_15")
-# print(f' eto converted_file_path_from_stroka_15: {converted_file_path.json()}')
-#
-#
-#
-# #II resize_no_docker
-# converted_file_path = requests.post("http://127.0.0.1:8001/api/v1/modelhandlerapi/resize_no_docker/?file_path_to_convert")
-# print(f' eto converted_file_path: {converted_file_path.json()}')
-#
-#
-# converted_file_path = requests.post("http://127.0.0.1:8001/api/v1/modelhandlerapi/resize_no_docker/?file_path_to_convert_from_stroka_15")
-# print(f' eto converted_file_path_from_stroka_15: {converted_file_path.json()}')
+answer = requests.get("http://127.0.0.1:8001/api/v1/modelhandlerapi/get_model_object_by_id/?film_uuid=efc94832-a392-4d68-b117-f90b5080218d")
+file_path_to_convert = answer.json().get('file_path')
+file_path_to_convert_from_stroka_15 = 'http://127.0.0.1:8000/media/film_works/%D1%82%D0%B5%D1%81%D1%82_GI7BI9C.mp4'
+print(f' eto answer one user: {answer.json()}, file_path_to_convert : {file_path_to_convert} , {file_path_to_convert_from_stroka_15}')
+
+
+converted_file_path = requests.post("http://127.0.0.1:8001/api/v1/modelhandlerapi/resize/?file_path_to_convert")
+print(f' eto converted_file_path: {converted_file_path.json()}')
+
+
+converted_file_path = requests.post("http://127.0.0.1:8001/api/v1/modelhandlerapi/resize/?file_path_to_convert_from_stroka_15")
+print(f' eto converted_file_path_from_stroka_15: {converted_file_path.json()}')
+
+
+
+#II resize_no_docker
+converted_file_path = requests.post("http://127.0.0.1:8001/api/v1/modelhandlerapi/resize_no_docker/?file_path_to_convert")
+print(f' eto converted_file_path: {converted_file_path.json()}')
+
+
+converted_file_path = requests.post("http://127.0.0.1:8001/api/v1/modelhandlerapi/resize_no_docker/?file_path_to_convert_from_stroka_15")
+print(f' eto converted_file_path_from_stroka_15: {converted_file_path.json()}')
