d1298da547c159d434d5aafe52528ede2c78283d
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-09 14:53:33 +0300

    правки в resize

--- myconverter/alice/alice_api_test.py
+++ myconverter/alice/alice_api_test.py
@@ -12,6 +12,7 @@ print(f' eto answer.json for actor : {answer.json()}')
 string_dict = {'actor' : 'Зеленая миля'}
 url = 'http://127.0.0.1:8002/api/v1/alice_api/find_actor_in_fake_db_json/'
 response = requests.request("GET", url,  params=string_dict)
+response =  requests.get(url,  params=string_dict)
 print(f' eto response.json for actor : {response.json()}')
 
 #2 просто возвращаю fake_films_db
