51b9266da005ec29ee248857cca02e079ad2856e
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-09 14:52:40 +0300

    правки в resize

--- myconverter/alice/alice_api_test.py
+++ myconverter/alice/alice_api_test.py
@@ -1,7 +1,6 @@
 import requests
 string = 'Зеленая миля'
-string_dict = {'actor' : 'Зеленая миля'}
-url = 'http://127.0.0.1:8002/api/v1/alice_api/find_actor_in_fake_db_json/'
+
 
 answer = requests.get(f"http://127.0.0.1:8002/api/v1/alice_api/find_actor_in_fake_db/?input_actor={string}")
 print(f' eto answer.json for actor : {answer.json()}')
@@ -10,7 +9,8 @@ print(f' eto answer.json for actor : {answer.json()}')
 # answer = requests.get(f"http://127.0.0.1:8002/api/v1/alice_api/find_actor_in_fake_db_json/?input_actor={string_dict}")
 # print(f' eto answer.json for actor : {answer.json()}')
 
-
+string_dict = {'actor' : 'Зеленая миля'}
+url = 'http://127.0.0.1:8002/api/v1/alice_api/find_actor_in_fake_db_json/'
 response = requests.request("GET", url,  params=string_dict)
 print(f' eto response.json for actor : {response.json()}')
 
