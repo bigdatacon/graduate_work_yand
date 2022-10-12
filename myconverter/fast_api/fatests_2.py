625e4ca7a49c666587af46dac65a098691522c4e
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-12 21:28:54 +0300

    правки в resize

--- myconverter/fast_api/fatests_2.py
+++ myconverter/fast_api/fatests_2.py
@@ -21,37 +21,37 @@ except Exception as e:
 
 print(f'закончил resize в ручке resize_full')
 
-#1 скачиваю файл, путь до видео идет почему то c django - но так сеть не видит, поэтому делаю replace на 127.0.0.1
-# film_uuid="9f4bc97f-917c-4f1d-b099-cd1d16ec7269"
-# answer = requests.get("http://127.0.0.1:8001/api/v1/modelhandlerapi/get_model_object_by_id/?film_uuid=9f4bc97f-917c-4f1d-b099-cd1d16ec7269")
-answer = requests.get(f"http://127.0.0.1:8001/api/v1/modelhandlerapi/get_model_object_by_id/?film_uuid={film_uuid}")
-print(f'here answer.json for get_model_object_by_id : {answer.json()}')
-print(f'here answer.json detail for get_model_object_by_id : {answer.json().get("file_path").replace("django", "127.0.0.1")}')
-file_path_from_database = answer.json().get("file_path").replace('django', '127.0.0.1')
-output_file_path = f"{uuid.uuid4()}loaded.mp4"
-# url = 'https://www.facebook.com/favicon.ico'
-r = requests.get(file_path_from_database, allow_redirects=True)
-open(f'./loaded/{output_file_path}', 'wb').write(r.content)
-print('WRITE')
-
-
-
-
-#2 закидываю полученный файл в resize
-# file_path = os.path.join("./loaded", "dd9b7e11-4c4d-4af6-93cf-bc9e9d62ace6loaded.mp4")
-file_path = os.path.join(f'./loaded/{output_file_path}')
-fd = open(file_path, 'rb')
-try:
-    print(f'start resize')
-    response = requests.post("http://127.0.0.1:8001/api/v1/modelhandlerapi/resize/",
-                             files={'input_file_path': fd}, file_id=film_uuid)
-    print(f"RESULT RESIZE ....")
-    file_path_after_resize = response.json()
-    print(f'file_path_after_resize : {file_path_after_resize}' )
-except Exception as e:
-    print(f'except in resize : {e.args}')
-
-fd.close()
+# #1 скачиваю файл, путь до видео идет почему то c django - но так сеть не видит, поэтому делаю replace на 127.0.0.1
+# # film_uuid="9f4bc97f-917c-4f1d-b099-cd1d16ec7269"
+# # answer = requests.get("http://127.0.0.1:8001/api/v1/modelhandlerapi/get_model_object_by_id/?film_uuid=9f4bc97f-917c-4f1d-b099-cd1d16ec7269")
+# answer = requests.get(f"http://127.0.0.1:8001/api/v1/modelhandlerapi/get_model_object_by_id/?film_uuid={film_uuid}")
+# print(f'here answer.json for get_model_object_by_id : {answer.json()}')
+# print(f'here answer.json detail for get_model_object_by_id : {answer.json().get("file_path").replace("django", "127.0.0.1")}')
+# file_path_from_database = answer.json().get("file_path").replace('django', '127.0.0.1')
+# output_file_path = f"{uuid.uuid4()}loaded.mp4"
+# # url = 'https://www.facebook.com/favicon.ico'
+# r = requests.get(file_path_from_database, allow_redirects=True)
+# open(f'./loaded/{output_file_path}', 'wb').write(r.content)
+# print('WRITE')
+#
+#
+#
+#
+# #2 закидываю полученный файл в resize
+# # file_path = os.path.join("./loaded", "dd9b7e11-4c4d-4af6-93cf-bc9e9d62ace6loaded.mp4")
+# file_path = os.path.join(f'./loaded/{output_file_path}')
+# fd = open(file_path, 'rb')
+# try:
+#     print(f'start resize')
+#     response = requests.post("http://127.0.0.1:8001/api/v1/modelhandlerapi/resize/",
+#                              files={'input_file_path': fd}, file_id=film_uuid)
+#     print(f"RESULT RESIZE ....")
+#     file_path_after_resize = response.json()
+#     print(f'file_path_after_resize : {file_path_after_resize}' )
+# except Exception as e:
+#     print(f'except in resize : {e.args}')
+#
+# fd.close()
 
 
 #3 Закидываю файл полученный от resize в таблицу fileupload
