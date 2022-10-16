15ef980fcfa30c325b2828cb47ea6162c39f547a
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-16 12:33:13 +0300

    правки в resize

--- myconverter/fast_api/fatests_2.py
+++ myconverter/fast_api/fatests_2.py
@@ -2,7 +2,8 @@
 import requests
 import os
 
-film_uuid = os.getenv('FILM_TEST_UUID', "9f4bc97f-917c-4f1d-b099-cd1d16ec7269")
+# film_uuid = os.getenv('FILM_TEST_UUID', "9f4bc97f-917c-4f1d-b099-cd1d16ec7269")
+film_uuid = os.getenv('FILM_TEST_UUID', "85229cbf-b09d-4f19-beb0-e47baa3c0c93")
 try:
     answer = requests.post(f"http://127.0.0.1:8001/api/v1/modelhandlerapi/resize_full/?file_id={film_uuid}")
     print(f'here answer.json for resize_full : {answer.json()}')
