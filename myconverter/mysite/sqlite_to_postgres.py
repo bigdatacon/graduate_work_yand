4ed087982e4750831c21106fd61527412fa05114
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-10-19 20:51:26 +0300

    правки в resize

--- myconverter/mysite/sqlite_to_postgres.py
+++ myconverter/mysite/sqlite_to_postgres.py
@@ -205,6 +205,8 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
 
 if __name__ == '__main__':
     print(datetime.datetime.now())
-    dsl = {'dbname': 'movies', 'user': 'postgres', 'password': 123, 'host': 'postgres', 'port': 5432}
+    # dsl = {'dbname': 'movies', 'user': 'postgres', 'password': 123, 'host': 'postgres', 'port': 5432}
+    dsl = {'dbname': 'converter', 'user': 'postgres', 'password': 123, 'host': 'postgres', 'port': 5432}
+
     with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
