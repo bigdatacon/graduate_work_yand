99085590058c0f7b286dc914121665b425b5c20e
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-11-01 07:05:56 +0300

    Перенес views и serializer из 1 спринта

--- myconverter/mysite/mysite/urls.py
+++ myconverter/mysite/mysite/urls.py
@@ -1,15 +1,22 @@
 from django.urls import include, path
 from rest_framework import routers
 from polls import views
+from movies import views
+from movies.views import FilmWorkMovieViewSet, MovieList, filmworkmovie_list, index
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+# from django.urls import include, path
 
 router = routers.DefaultRouter()
 router.register(r'fileupload', views.FileuplViewSet)
 router.register(r'filmwork', views.FilmWorkViewSet)
 
-from django.contrib import admin
-# from django.urls import include, path
+#Блок для таблицы с фильмами 1000+
+router.register(r'FilmWorkMovie', FilmWorkMovieViewSet)
+
+
+
 
 urlpatterns = [
     # path('polls/question/', include('polls.urls')),
@@ -17,7 +24,16 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+
+    #Блок для таблицы с фильмами 1000+
+    path('film_workmovie', views.index, name='film_workmovie'),
+    path('filmworkmovie_list/', views.filmworkmovie_list),
+    path('filmworkmovie_view/', views.MovieList.as_view()),
+    path('index/', views.index),
+
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
 
 urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
