af0f40e4ac193b93c05eac8a855270567f9b02f2
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-11-05 18:43:02 +0300

    Перенес views и serializer из 1 спринта

--- myconverter/mysite/mysite/urls.py
+++ myconverter/mysite/mysite/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
-from polls import views
-from movies import views
+# from polls import views
+from polls.views import FileuplViewSet, FilmWorkViewSet
+# from movies import views
 from movies.views import FilmWorkMovieViewSet, MovieList, filmworkmovie_list, index
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,8 +10,11 @@ from django.contrib import admin
 # from django.urls import include, path
 
 router = routers.DefaultRouter()
-router.register(r'fileupload', views.FileuplViewSet)
-router.register(r'filmwork', views.FilmWorkViewSet)
+# router.register(r'fileupload', views.FileuplViewSet)
+# router.register(r'filmwork', views.FilmWorkViewSet)
+
+router.register(r'fileupload', FileuplViewSet)
+router.register(r'filmwork', FilmWorkViewSet)
 
 #Блок для таблицы с фильмами 1000+
 router.register(r'FilmWorkMovie', FilmWorkMovieViewSet)
@@ -26,10 +30,15 @@ urlpatterns = [
     path('', include(router.urls)),
 
     #Блок для таблицы с фильмами 1000+
-    path('film_workmovie', views.index, name='film_workmovie'),
-    path('filmworkmovie_list/', views.filmworkmovie_list),
-    path('filmworkmovie_view/', views.MovieList.as_view()),
-    path('index/', views.index),
+    # path('film_workmovie', views.index, name='film_workmovie'),
+    # path('filmworkmovie_list/', views.filmworkmovie_list),
+    # path('filmworkmovie_view/', views.MovieList.as_view()),
+    # path('index/', views.index),
+
+    path('film_workmovie', index, name='film_workmovie'),
+    path('filmworkmovie_list/', filmworkmovie_list),
+    path('filmworkmovie_view/', MovieList.as_view()),
+    path('index/', index),
 
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
